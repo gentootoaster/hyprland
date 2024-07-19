3598f7b060e438fd5c4394ec0208c0bd427c1811
gentootoaster <lobachev.arseniy@gmail.com>
2024-07-19 17:30:55 +0500

    Add files via upload

---
+++ waybar/mediaplayer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+player_status=$(playerctl status)
+if [ "$player_status" = "Playing" ]; then
+    echo "$(playerctl metadata artist) - $(playerctl metadata title)"
+elif [ "$player_status" = "Pause" ]; then
+    echo " $(playerctl metadata artist) - $(playerctl metadata title)"
+else
+    echo "Let's dance!" 
+fi
